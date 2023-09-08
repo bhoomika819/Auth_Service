821ab7f6d2b77f5a4c23f212f50f3f713b514a92
bhoomika819 <batribhoomika@gmail.com>
2023-09-08 22:14:45 +0530

    added get user by ud un repo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 // const {User}  = require('./models/index');
 // const bcrypt = require('bcrypt');
 
+//const UserRepository = require('./repository/user_repository');
+
 const app = express();
 
 const prepareAndServer = () => {
@@ -14,6 +16,9 @@ const prepareAndServer = () => {
 
     app.listen(PORT,async () => {
     console.log(`Server Started on PORT : ${PORT}`);
+    // const repo = new UserRepository();
+    // const response = await repo.getById(1);
+    // console.log(response);
     // const incomingpassword = 'Bhoomika@321';
     // const user = await User.findByPk(2);
     // const response = bcrypt.compareSync(incomingpassword,user.password);
